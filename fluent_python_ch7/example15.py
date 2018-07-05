740cdc44d1bbe9e6fbc30c9b4de67cdac0929090
root <takekazusa@gmail.com>
2018-07-05 11:42:37 +0000

    mod

--- fluent_python_ch7/example15.py
+++ fluent_python_ch7/example15.py
@@ -2,14 +2,25 @@
 Fluent Python Ch.7 example15
 """
 import time 
+import functools.wrap
+
 
 def clock(func):
-    def clocked(*args):
+    @functools.wrap(func)
+    def clocked(*args, **kwargs):
         t0 = time.perf_counter()
         result = func(*args)
         elapsed = time.perf_counter() - t0
         name = func.__name__
-        arg_star = ', '.join(repr(arg) for arg in args)
-        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_star, result))
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
         return result
     return clocked
+
+
